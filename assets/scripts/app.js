dea13edf7d8c679f6f81920e278ba499fb2b1707
Pradeep <pradeepkumarhe1989@gmail.com>
2023-07-22 17:28:35 +0530

    Arrow function Examples

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,34 @@
-function greet(userName,message){
-   return "Hi "+userName+", "+message;
+let greetMessageWithFunctionKeyword=function(){
+    return "Hello world with function keyword";
 }
-console.log(greet("Pradeep","How are you?"));
-const greet1=greet("Javascript Developer","How are you?");
-console.log(greet1);
 
+let greetMessageWithoutFunctionKeyword=()=>{
+    return "Hello world without function keyword";
+}
+
+let result=(userName,message)=>{
+    return "Hi "+userName+", "+message;
+}
+
+let addition=(a,b)=>{
+    return a+b;
+}
+
+let multiplication= a =>{
+    return a*3;
+}
+
+let division=a=>a/3;
+
+let getNewObj=number=>({age:number});
+
+let getEmpObj=(name,designation,salary)=>({name:name,designation:designation,salary:salary});
+
+console.log(greetMessageWithFunctionKeyword());
+console.log(greetMessageWithoutFunctionKeyword());
+console.log(result("Pradeep","How are you?"));
+console.log("Addtion of two numbers is,"+addition(2,3));
+console.log("2 * 3 is,"+multiplication(2));
+console.log("9 / 3 is,"+division(9));
+console.log(getNewObj(34));
+console.log(getEmpObj("Pradeep","Javascript Developer","35,000"));

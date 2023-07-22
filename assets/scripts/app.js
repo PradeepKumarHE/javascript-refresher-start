a4dc2370d43ab4ee50c1d2b04330fa429c36a8a3
Pradeep <pradeepkumarhe1989@gmail.com>
2023-07-22 21:34:38 +0530

    Manipulating DOM

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,18 +1,2 @@
-const hobbies=["Sports","Cooking"];
-const newhobbies=["Reading"];
-const mergedhobbiesWithoutSpread=[hobbies,newhobbies];
-console.log(mergedhobbiesWithoutSpread);
-const mergedhobbiesWithSpread=[...hobbies,...newhobbies];
-console.log(mergedhobbiesWithSpread);
-
-const user={
-    name:"Pradeep",
-    technolgy:"JavaScript",
-};
-const extendedUser={
-    isAdmin:true,
-    ...user
-};
-console.log(extendedUser);
-
-
+const list=document.querySelector("ul");
+list.remove();
